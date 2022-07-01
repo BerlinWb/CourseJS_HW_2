85edf7b1935e3846da72ff631de3f6f7c0d4c518
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-01 20:20:32 +0300

    Add files via upload

---
+++ Course_Js_Task2/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// task_1
+let admin;
+let name = "John";
+admin = name;
+
+alert(admin);
+
+// task_2
+let Earth;
+let CurrentSiteGuest;
+
+// task_3
+let num = "10" + 10;
+console.log(typeof num);
